dfbf4713b7ec0acab9a11c3c8fa0b207a30c50dd
Sergi Martínez <sergi.martinez@gmail.com>
2017-11-13 18:54:22 +0100

    Initial app

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_planets_workshop
+
+A workshop on Flutter
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Planets',
+      home: new Container(
+        child: new FlutterLogo(),
+      )
+    );
+  }
+}
