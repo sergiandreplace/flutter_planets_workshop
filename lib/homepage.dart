26a21f315de2fa641c1a01213498caf15bb1a240
Sergi Martínez <sergi.martinez@gmail.com>
2017-11-13 19:37:18 +0100

    planetrow with card created

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,6 +4,7 @@ class HomePage extends StatelessWidget {
 
   final String _title;
   final List<String> _planets = ["Mars", "Neptune", "Earth", "Venus", "Moon"];
+
   HomePage(this._title);
 
   @override
@@ -14,10 +15,33 @@ class HomePage extends StatelessWidget {
       ),
       body: new Center(
         child: new ListView.builder(
-          itemBuilder: (context, index) => new Text(_planets[index]),
+          itemBuilder: (context, index) => new PlanetRow(_planets[index]),
           itemCount: _planets.length,
+          padding: new EdgeInsets.symmetric(vertical: 16.0),
         ),
       ),
+    );
+  }
+}
+
+class PlanetRow extends StatelessWidget {
+
+  final String _planet;
+
+  PlanetRow(this._planet);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: new EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      child: new Card(
+        child: new Container(
+          alignment: Alignment.center,
+          child: new Text(_planet),
+          height: 50.0,
+        ),
+      ),
+
     );
   }
 }
